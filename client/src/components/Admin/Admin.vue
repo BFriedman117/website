aebdc81b7aec51bba3faf7116932297c65761157
BFriedman117 <binyaminf@yahoo.com>
2019-03-04 17:35:09 -0500

    touched up styling on admin page

--- client/src/components/Admin/Admin.vue
+++ client/src/components/Admin/Admin.vue
@@ -1,66 +1,17 @@
 <template>
   <div>
     <div>
-      <h1>This page is for Admin Only! If you are not the creator of this website, kindly leave! #websecurity #buildawall #cyber #getout!</h1>
+      <h3>This page is for Admin Only! If you are not the creator of this website, kindly leave! #websecurity #buildawall #cyber #getout!</h3>
     </div>
-
-    <!-- Post Editor -->
-    <div style="padding: 30px;">
-      <div>
-        <label>Headline</label>
-        <input v-model="headline"/>
-      </div>
-      <div>
-        <label>Subheader</label>
-        <input v-model="subheader"/>
-      </div>
-      <div>
-        <label>Content</label>
-        <VueEditor v-model="body"/>
-      </div>
-      <div style="max-width: 400px;">
-        <Multiselect v-model="contentTag" :options="getTags" label="type"></Multiselect>
-      </div>
-      <div>
-        <button @click="savePost" :disabled="!contentTag">Save Post</button>
-      </div>
-    </div>
+    <PostEditor></PostEditor>
   </div>
 </template>
 <script>
-import { VueEditor } from 'vue2-editor'
-import { Multiselect } from 'vue-Multiselect'
-import { mapGetters } from 'vuex'
+import PostEditor from './PostEditor'
 
 export default {
   components: {
-    VueEditor,
-    Multiselect
-  },
-  data () {
-    return {
-      headline: '',
-      subheader: '',
-      body: '',
-      contentTag: null
-    }
-  },
-  methods: {
-    savePost () {
-      let post = {
-        post: {
-          headline: this.headline,
-          subheader: this.subheader,
-          body: this.body
-        },
-        contentTag: this.contentTag
-      }
-      this.$store.dispatch('addPost', post)
-    }
-  },
-  computed: {
-    ...mapGetters(['getTags'])
+    PostEditor
   }
 }
 </script>
-<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
---
+++ client/src/components/Admin/PostEditor.vue
@@ -0,0 +1,96 @@
+<template>
+  <!-- Post Editor -->
+  <div class="editor-container">
+
+    <div class="form-group">
+      <label>
+        <strong>
+          Headline:
+        </strong>
+      </label>
+      <input v-model="headline" class="form-input"/>
+    </div>
+
+    <div class="form-group">
+      <label>
+        <strong>
+          Subheader:
+        </strong>
+      </label>
+      <input v-model="subheader" class="form-input"/>
+    </div>
+
+    <div class="form-group">
+      <label>
+        <strong>
+          Content:
+        </strong>
+      </label>
+      <VueEditor v-model="body" style="width: 100%;"/>
+    </div>
+
+    <div class="form-group">
+      <label>
+        <strong>
+          Category:
+        </strong>
+      </label>
+      <div style="width: 400px;">
+        <Multiselect v-model="contentTag" :options="getTags" label="type"></Multiselect>
+      </div>
+    </div>
+    <div>
+      <button @click="savePost" :disabled="!contentTag">Save Post</button>
+    </div>
+  </div>
+</template>
+<script>
+import { VueEditor } from 'vue2-editor'
+import { Multiselect } from 'vue-Multiselect'
+import { mapGetters } from 'vuex'
+
+export default {
+  components: {
+    VueEditor,
+    Multiselect
+  },
+  data () {
+    return {
+      headline: '',
+      subheader: '',
+      body: '',
+      contentTag: null
+    }
+  },
+  methods: {
+    savePost () {
+      let post = {
+        post: {
+          headline: this.headline,
+          subheader: this.subheader,
+          body: this.body
+        },
+        contentTag: this.contentTag
+      }
+      this.$store.dispatch('addPost', post)
+    }
+  },
+  computed: {
+    ...mapGetters(['getTags'])
+  }
+}
+</script>
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
+<style>
+  .form-group {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 15px;
+  }
+
+  .form-input {
+    width: 400px;
+    font-size: 24px;
+  }
+</style>
